2b50bc0869b9a7191183cf325d21611a8e4548ec
AlejandroLopezDeBlas <alejandro.lopezdblas@iesfm.onmicrosoft.com>
2022-09-16 10:27:10 +0200

    mi primera ventana

---
+++ src/org/iesfm/ventana/main.java
@@ -0,0 +1,28 @@
+package org.iesfm.ventana;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class main {
+    public static void main(String[] args) {
+        JFrame f  = new JFrame();
+        f.setBounds(10, 10, 200, 300);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        JPanel mainPanel = new JPanel();
+
+        JLabel label = new JLabel("Usuario");
+        mainPanel.add(label);
+
+        JTextField t = new JTextField(16);
+        mainPanel.add(t);
+
+        JButton b = new JButton("Aceptar");
+        mainPanel.add(b);
+
+        f.setContentPane(mainPanel);
+
+
+    }
+}
