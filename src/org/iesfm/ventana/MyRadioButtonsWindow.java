e3f1663eadda093b4cde9e323336d8eb5ba60651
AlejandroLopezDeBlas <alejandro.lopezdblas@iesfm.onmicrosoft.com>
2022-09-19 11:06:01 +0200

    My radio buttons

--- src/org/iesfm/ventana/MyRadioButtonsWindow.java
+++ src/org/iesfm/ventana/MyRadioButtonsWindow.java
@@ -1,17 +1,16 @@
 package org.iesfm.ventana;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class MyRadioButtonsWindow {
     public static void main(String[] args) {
+
         JFrame f = new JFrame();
         f.setBounds(10, 10, 200, 300);
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel mainpanel = new JPanel();
-        mainpanel.setBackground(Color.white);
 
         JRadioButton boton1 = new JRadioButton("Linux");
         boton1.setBounds(75,50, 100, 30);
@@ -19,13 +18,25 @@ public class MyRadioButtonsWindow {
         boton2.setBounds(150,50, 100, 30);
         JRadioButton boton3 = new JRadioButton("Macintosh");
         boton3.setBounds(225,50, 100, 30);
+
         ButtonGroup grupo = new ButtonGroup();
+
         grupo.add(boton1);
         grupo.add(boton2);
         grupo.add(boton3);
+
         mainpanel.add(boton1);
         mainpanel.add(boton2);
         mainpanel.add(boton3);
+
+        JLabel imagen = new JLabel();
+        imagen.setIcon(new ImageIcon ("descarga.jpeg"));
+
+        mainpanel.add(imagen);
+
+        JButton aceptar = new JButton("Aceptar");
+        mainpanel.add(aceptar);
+
         f.setContentPane(mainpanel);
     }
 }
