44965e3d35d847ce4483c9ae1da3c97705261e9c
AlejandroLopezDeBlas <alejandro.lopezdblas@iesfm.onmicrosoft.com>
2022-09-19 10:36:17 +0200

    My radio buttons

---
+++ src/org/iesfm/ventana/MyRadioButtonsWindow.java
@@ -0,0 +1,25 @@
+package org.iesfm.ventana;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyRadioButtonsWindow {
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        f.setBounds(10, 10, 200, 300);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel mainpanel = new JPanel();
+        mainpanel.setBackground(Color.white);
+        JRadioButton boton1 = new JRadioButton("Hombre");
+        boton1.setBounds(75,50, 100, 30);
+        JRadioButton boton2 = new JRadioButton("Mujer");
+        boton2.setBounds(200,50, 100, 30);
+        ButtonGroup grupo = new ButtonGroup();
+        grupo.add(boton1);
+        grupo.add(boton2);
+        mainpanel.add(boton1);
+        mainpanel.add(boton2);
+        f.setContentPane(mainpanel);
+    }
+}
