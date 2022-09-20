39dddbdf0f681e7d84148aa2c79db9da92413a05
AlejandroLopezDeBlas <alejandro.lopezdblas@iesfm.onmicrosoft.com>
2022-09-20 12:41:31 +0200

    ComboBox y TitledBorder

--- src/org/iesfm/ventana/MyTitledBorderWindow.java
+++ src/org/iesfm/ventana/MyTitledBorderWindow.java
@@ -1,6 +1,7 @@
 package org.iesfm.ventana;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MyTitledBorderWindow {
     public static void main(String[] args) {
@@ -9,19 +10,30 @@ public class MyTitledBorderWindow {
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel mainpanel = new JPanel();
-        mainpanel.setBounds(10, 10, 10, 20);
+        JPanel panel = new JPanel();
 
-        mainpanel.setBorder(BorderFactory.createTitledBorder("Search"));
+
+        JPanel mainpanel = new JPanel();
+        panel.setBounds(40, 40, 400, 100);
+
+        panel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.gray, 10, true), "Search"));
 
         JRadioButton boton1 = new JRadioButton("By Name");
 
         JRadioButton boton2 = new JRadioButton("By ID");
 
-        mainpanel.add(boton1);
-        mainpanel.add(boton2);
+        ButtonGroup grupo = new ButtonGroup();
 
+        mainpanel.add(panel);
+
+        grupo.add(boton1);
+        grupo.add(boton2);
+
+        panel.add(boton1);
+        panel.add(boton2);
 
         f.setContentPane(mainpanel);
+        f.repaint();
+        f.revalidate();
     }
 }
