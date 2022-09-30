6eaf73afbbeebb3c5f4f2e772d5d96c65cefc4f9
AlejandroLopezDeBlas <alejandro.lopezdblas@iesfm.onmicrosoft.com>
2022-09-30 10:39:36 +0200

    BorderLayout y FlowLayout

--- src/org/iesfm/ventana/Calculator2Window.java
+++ src/org/iesfm/ventana/Calculator2Window.java
@@ -6,73 +6,69 @@ import java.awt.*;
 public class Calculator2Window {
     public static void main(String[] args) {
         JFrame f = new JFrame();
-        f.setBounds(10, 10, 200, 300);
+        f.setBounds(10, 10, 300, 300);
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel mainPanel = new JPanel();
+        JPanel mainPanel = new JPanel(new BorderLayout());
 
-        JPanel buttonPanel = new JPanel();
+        JPanel buttonPanel = new JPanel(new BorderLayout(10, 10));
+        buttonPanel.setBackground(Color.GRAY);
+        buttonPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 
-        buttonPanel.setLayout(new BorderLayout());
+        JPanel buttonPanel1 = new JPanel(new GridLayout(4, 4, 10, 10));
+        buttonPanel1.setOpaque(false);
 
-        JPanel buttonPanel1 = new JPanel();
-
-        JPanel buttonPanel2 = new JPanel();
-
-        GridLayout gridLayout = new GridLayout(5, 4);
-        gridLayout.setVgap(10);
-        gridLayout.setHgap(10);
-
-        GridLayout gridLayout1 = new GridLayout(1, 2);
-        gridLayout1.setVgap(10);
-        gridLayout1.setHgap(10);
-
-        buttonPanel1.setLayout(gridLayout);
-        buttonPanel2.setLayout(gridLayout1);
+        JPanel buttonPanel2 = new JPanel(new GridLayout(1, 2, 10, 10));
+        buttonPanel2.setPreferredSize(new Dimension(50, 50));
+        buttonPanel2.setOpaque(false);
 
         JButton button = new JButton("7");
-        buttonPanel1.add(button, gridLayout);
+        buttonPanel1.add(button);
         JButton button1 = new JButton("8");
-        buttonPanel1.add(button1, gridLayout);
+        buttonPanel1.add(button1);
         JButton button2 = new JButton("9");
-        buttonPanel1.add(button2, gridLayout);
+        buttonPanel1.add(button2);
         JButton button3 = new JButton("DEL");
         button3.setBackground(Color.CYAN);
-        buttonPanel1.add(button3, gridLayout);
+        buttonPanel1.add(button3);
+
         JButton button4 = new JButton("4");
-        buttonPanel1.add(button4, gridLayout);
+        buttonPanel1.add(button4);
         JButton button5 = new JButton("5");
-        buttonPanel1.add(button5, gridLayout);
+        buttonPanel1.add(button5);
         JButton button6 = new JButton("6");
-        buttonPanel1.add(button6, gridLayout);
+        buttonPanel1.add(button6);
         JButton button7 = new JButton("+");
-        buttonPanel1.add(button7, gridLayout);
+        buttonPanel1.add(button7);
+
         JButton button8 = new JButton("1");
-        buttonPanel1.add(button8, gridLayout);
+        buttonPanel1.add(button8);
         JButton button9 = new JButton("2");
-        buttonPanel1.add(button9, gridLayout);
+        buttonPanel1.add(button9);
         JButton button10 = new JButton("3");
-        buttonPanel1.add(button10, gridLayout);
+        buttonPanel1.add(button10);
         JButton button11 = new JButton("-");
-        buttonPanel1.add(button11, gridLayout);
+        buttonPanel1.add(button11);
+
         JButton button12 = new JButton(".");
-        buttonPanel1.add(button12, gridLayout);
+        buttonPanel1.add(button12);
         JButton button13 = new JButton("0");
-        buttonPanel1.add(button13, gridLayout);
+        buttonPanel1.add(button13);
         JButton button14 = new JButton("/");
-        buttonPanel1.add(button14, gridLayout);
+        buttonPanel1.add(button14);
         JButton button15 = new JButton("x");
-        buttonPanel1.add(button15, gridLayout);
+        buttonPanel1.add(button15);
+
         JButton button16 = new JButton("RESET");
         button16.setBackground(Color.CYAN);
-        buttonPanel2.add(button16, gridLayout1);
+        buttonPanel2.add(button16);
         JButton button17 = new JButton("=");
         button17.setBackground(Color.ORANGE);
-        buttonPanel2.add(button17, gridLayout1);
+        buttonPanel2.add(button17);
 
-        buttonPanel.add(buttonPanel1, BorderLayout.NORTH);
-        buttonPanel.add(buttonPanel2, BorderLayout.CENTER);
+        buttonPanel.add(buttonPanel1, BorderLayout.CENTER);
+        buttonPanel.add(buttonPanel2, BorderLayout.SOUTH);
 
 
         JPanel panel2 = new JPanel();
@@ -84,6 +80,7 @@ public class Calculator2Window {
         mainPanel.add(panel2, BorderLayout.NORTH);
         mainPanel.add(buttonPanel, BorderLayout.CENTER);
 
+        f.setTitle("Calculadora");
         f.setContentPane(mainPanel);
         f.repaint();
         f.revalidate();
--- src/org/iesfm/ventana/CalculatorWindow.java
+++ src/org/iesfm/ventana/CalculatorWindow.java
@@ -6,69 +6,71 @@ import java.awt.*;
 public class CalculatorWindow {
     public static void main(String[] args) {
         JFrame f = new JFrame();
-        f.setBounds(10, 10, 200, 300);
+        f.setBounds(10, 10, 500, 300);
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel mainPanel = new JPanel();
+        JPanel mainPanel = new JPanel(new BorderLayout());
 
         JPanel panel1 = new JPanel();
 
-        GridLayout gridLayout = new GridLayout(5, 4);
-        gridLayout.setVgap(10);
-        gridLayout.setHgap(10);
+        GridLayout gridLayout = new GridLayout(5, 4, 10, 10);
 
         panel1.setLayout(gridLayout);
 
         JButton button = new JButton("Rtc");
-        panel1.add(button, gridLayout);
+        panel1.add(button);
         JButton button1 = new JButton("CE");
-        panel1.add(button1, gridLayout);
+        panel1.add(button1);
         JButton button2 = new JButton("CL");
-        panel1.add(button2, gridLayout);
+        panel1.add(button2);
         JButton button3 = new JButton("+/-");
-        panel1.add(button3, gridLayout);
+        panel1.add(button3);
         JButton button4 = new JButton("7");
-        panel1.add(button4, gridLayout);
+        panel1.add(button4);
+
         JButton button5 = new JButton("8");
-        panel1.add(button5, gridLayout);
+        panel1.add(button5);
         JButton button6 = new JButton("9");
-        panel1.add(button6, gridLayout);
+        panel1.add(button6);
         JButton button7 = new JButton("/");
-        panel1.add(button7, gridLayout);
+        panel1.add(button7);
         JButton button8 = new JButton("4");
-        panel1.add(button8, gridLayout);
+        panel1.add(button8);
         JButton button9 = new JButton("5");
-        panel1.add(button9, gridLayout);
+        panel1.add(button9);
+
         JButton button10 = new JButton("6");
-        panel1.add(button10, gridLayout);
+        panel1.add(button10);
         JButton button11 = new JButton("x");
-        panel1.add(button11, gridLayout);
+        panel1.add(button11);
         JButton button12 = new JButton("1");
-        panel1.add(button12, gridLayout);
+        panel1.add(button12);
         JButton button13 = new JButton("2");
-        panel1.add(button13, gridLayout);
+        panel1.add(button13);
         JButton button14 = new JButton("3");
-        panel1.add(button14, gridLayout);
+        panel1.add(button14);
+
         JButton button15 = new JButton("-");
-        panel1.add(button15, gridLayout);
+        panel1.add(button15);
         JButton button16 = new JButton("0");
-        panel1.add(button16, gridLayout);
+        panel1.add(button16);
         JButton button17 = new JButton(".");
-        panel1.add(button17, gridLayout);
+        panel1.add(button17);
         JButton button18 = new JButton("=");
-        panel1.add(button18, gridLayout);
+        panel1.add(button18);
         JButton button19 = new JButton("+");
-        panel1.add(button19, gridLayout);
+        panel1.add(button19);
 
         JPanel panel2 = new JPanel();
 
-        JTextField textField = new JTextField(16);
+        JTextField textField = new JTextField(30);
         panel2.add(textField);
 
         mainPanel.add(panel2, BorderLayout.NORTH);
         mainPanel.add(panel1, BorderLayout.CENTER);
 
+        f.setTitle("Calculadora");
         f.setContentPane(mainPanel);
         f.repaint();
         f.revalidate();
